99e3b7d7d870c9044c0478779a24c0eed29ef46b
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-01-19 10:18:31 -0500

    Update script.js

--- web/main/static/script.js
+++ web/main/static/script.js
@@ -112,6 +112,7 @@ var cfg = {
     edgeColor: 'source',
     defaultNodeBorderColor: '#000',
     defaultNodeHoverColor: '#000',
+    defaultLabelHoverColor: '#000',
     edgeHoverColor: 'edge',
     edgeHoverSizeRatio: 1
 }
@@ -122,6 +123,9 @@ var s = new sigma({
   settings: {
 	defaultNodeColor: '#f1e28b', // yellow
 	defaultEdgeColor: '#bcc3cd', // blue gray
+    defaultLabelColor: '#ffffff00',
+    defaultLabelHoverColor: '#f1e28b',
+    defaultHoverLabelBGColor: '#ffffff00',
     edgeColor: 'source'
     // animationsTime: 1000	
   }
