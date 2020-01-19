83e10c962c4565547b1677f946d1406a5adecb63
TheAvidDev <AvidDev@protonmail.com>
2020-01-18 23:10:20 -0500

    Transfer part of code to packets.

--- app/hub.py
+++ app/hub.py
@@ -1,6 +1,8 @@
 from config import BUFFER_SIZE
 from socket import AF_INET, socket, SO_REUSEADDR, SOL_SOCKET, SOCK_STREAM
 from threading import Thread
+from packets.text_packet import TextPacket
+from packets.manager import PACKET_LIST
 
 clients = {}
 addresses = {}
@@ -28,7 +30,7 @@ class Hub:
         while True:
             client, client_address = self.server_socket.accept()
             print("%s:%s has connected." % client_address)
-            client.send(bytes("Name: ", "utf8"))
+            client.send(bytes([0x10]) + bytes("Name: ", "utf8"))
             addresses[client] = client_address
             Thread(target=self.handle_client, args=(client,)).start()
 
@@ -36,26 +38,28 @@ class Hub:
     def handle_client(self, client):
         name = client.recv(BUFFER_SIZE).decode("utf8")
         welcome = 'Welcome %s!' % name
-        client.send(bytes(welcome, "utf8"))
-        msg = "%s has joined the chat!" % name
-        broadcast(bytes(msg, "utf8"))
+        #packet = TextPacket
+        client.send(bytes([0x10]) + bytes(welcome, "utf8"))
+        #msg = "%s has joined the chat!" % name
+        #broadcast(bytes(msg, "utf8"))
         clients[client] = name
         while True:
             msg = client.recv(BUFFER_SIZE)
             if msg != bytes("{quit}", "utf8"):
-                print('Message: ', name, '[',msg,']')
-                broadcast(msg, name+": ")
+                packet = PACKET_LIST[msg[0]](msg)
+                packet.server_receive()
+                packet.set_data(name + ": " + msg[1:].decode("utf8"))
+                packet.server_send(clients)
             else:
                 client.send(bytes("{quit}", "utf8"))
                 client.close()
                 del clients[client]
-                broadcast(bytes("%s has left the chat." % name, "utf8"))
+                broadcast(TextPacket(bytes("%s has left the chat." % name, "utf8")))
                 break
 
 
-def broadcast(msg, prefix=""):
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
+def broadcast(packet):
+    packet.server_send(self)
 
 
 if __name__ == "__main__":
--- app/packets/text_packet.py
+++ app/packets/text_packet.py
@@ -9,13 +9,17 @@ class TextPacket(Packet):
         self.data = data
 
     def client_receive(self):
-        sys.stdout.write('\r' + self.data + '\n>>> ')
+        sys.stdout.write('\r' + self.data[1:] + '\n>>> ')
 
     def client_send(self, node):
         node.client_socket.send(bytes([self.id]) + bytes(self.data, "utf8"))
 
     def server_receive(self):
-        sys.stdout.write('[M] ' + self.data.decode('utf8') + '')
+        sys.stdout.write('[M] ' + self.data[1:].decode('utf8') + '\n')
 
-    def server_send(self, hub):
-        pass
+    def server_send(self, clients):
+        for sock in clients:
+            sock.send(self.data)
+
+    def set_data(self, data):
+        self.data = bytes([self.id]) + bytes(data, "utf8")
