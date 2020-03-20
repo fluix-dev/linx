b76f3c97d75da8ef96a294d3186d3b21fbdc1a9a
TheAvidDev <AvidDev@protonmail.com>
2020-03-20 18:46:34 -0400

    Use django urls and views

--- web/main/urls.py
+++ web/main/urls.py
@@ -4,7 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('network', views.network, name='network'),
+    path('team', views.team, name='team'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- web/main/views.py
+++ web/main/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def network(request):
+    return render(request, 'network.html')
+
+def about(request):
+    return render(request, 'about.html')
