c10e74e221f4140b4eb6a1632169ab03dd48e00d
TheAvidDev <AvidDev@protonmail.com>
2020-03-20 18:46:50 -0400

    Add STATIC_ROOT constant

--- web/web/settings.py
+++ web/web/settings.py
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 SECURE_CONTENT_TYPE_NOSNIFF = False
