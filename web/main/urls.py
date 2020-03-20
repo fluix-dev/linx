8129534a1e5781e927c6af9f20dc371d6279e306
TheAvidDev <AvidDev@protonmail.com>
2020-03-20 18:38:23 -0400

    Properly load static files

--- web/main/urls.py
+++ web/main/urls.py
@@ -1,6 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
